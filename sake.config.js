04f4c612f5e02709438e344f5f66dd40ad9d6f63
Beka Rice <beka.rice@gmail.com>
2018-10-30 15:35:57 -0400

    Update Sake config

--- sake.config.js
+++ sake.config.js
@@ -1,4 +1,15 @@
 module.exports = {
 	framework: false,
-	deploy: 'wp'
+	deploy: 'wp',
+	tasks: {
+		makepot: {
+			reportBugsTo: 'https://wordpress.org/support/plugin/woocommerce-extra-product-sorting-options'
+		}
+	},
+	paths: {
+		src: '.',
+		exclude: [
+			"build"
+		]
+	}
 }
