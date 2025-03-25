280bb2fd982a8f815764ddc5dbe7cfb87cf6de88
Ashley Gibson <agibson@godaddy.com>
2025-03-25 10:41:55 +0000

    Revert "Extra Product Sorting Options for WooCommerce: 2.11.0 Versioning"
    This reverts commit b8a4162f03deef3ad20ee8dec6df0af3548b1511.

--- woocommerce-extra-product-sorting-options.php
+++ woocommerce-extra-product-sorting-options.php
@@ -5,7 +5,7 @@
  * Description: Rename default sorting and optionally extra product sorting options.
  * Author: SkyVerge
  * Author URI: http://www.skyverge.com/
- * Version: 2.11.0
+ * Version: 2.11.0-dev.1
  * Text Domain: woocommerce-extra-product-sorting-options
  * Domain Path: /i18n/languages/
  *
@@ -20,7 +20,7 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  *
  * WC requires at least: 3.9.4
- * WC tested up to: 9.7.1
+ * WC tested up to: 9.7.0
  */
 
 defined( 'ABSPATH' ) or exit;
@@ -43,7 +43,7 @@ class WC_Extra_Sorting_Options {
 
 
 	/** plugin version number */
-	const VERSION = '2.11.0';
+	const VERSION = '2.11.0-dev.1';
 
 	/** required WooCommerce version number */
 	const MIN_WOOCOMMERCE_VERSION = '3.9.4';
